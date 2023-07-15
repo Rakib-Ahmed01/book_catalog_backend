112716cba3f9b411e6101f209b9aa1f2ea0db190
RakibAhmed01 <rakibahmed34360@gmail.com>
2023-07-15 16:17:29 +0600

    feat: setup error handling

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { globalErrorHandler, notFoundHandler } from "./middlewares/errors";
 
 const app: Application = express();
 
 app.use([cors(), express.json(), express.urlencoded({ extended: true })]);
 
-// TESTING
+// Home Route
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Welcome to Book Catalog Server...🐱‍🏍" });
 });
 
 // error handlers
+app.use(notFoundHandler);
+app.use(globalErrorHandler);
 
 export default app;
